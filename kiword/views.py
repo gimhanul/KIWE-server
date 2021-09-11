b5bb3757dfce6839887c00b1e1a68cd1e2682ef2
gimhanul <gimhanuly@gmail.com>
2021-09-11 22:57:54 +0900

    New: now show choices

--- kiword/views.py
+++ kiword/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Choice, Question
 
 def q(request, question_id):
     question = Question.objects.get(id=question_id)
-    context = {'question': question}
+    choices = Choice.objects.filter(question_id=question_id)
+    context = {
+        'question': question,
+        'choices': choices,
+        }
     return render(request, 'q.html', context)
