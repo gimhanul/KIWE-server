2143e2dce603df3510534ff0695c3451246c8906
gimhanul <gimhanuly@gmail.com>
2021-10-01 22:46:55 +0900

    Error can't load keywords when page load

--- kiword/views.py
+++ kiword/views.py
@@ -27,37 +27,34 @@ class Rank():
         self.score = score
 
     def __repr__(self):
-        return repr((self.keyword_id, self.score))
+        return repr((self.score, self.keyword_id))
 
 
 def kiword(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        one = data['one']
-        two = data['two']
-        three = data['three']
-        today = date.today()
-        birth = request.user.birth
-        birth = (today.year - birth.year + 1)//10
-        keywords = KeywordRelated.objects.filter(question='10', choice=birth).values('keyword')
-        
-        recomm = []
+    data = json.loads(request.body)
+    print(data)
+    one = data['one']
+    two = data['two']
+    three = data['three']
+    today = date.today()
+    birth = request.user.birth
+    birth = (today.year - birth.year + 1)//10
+    keywords = KeywordRelated.objects.filter(question='10', choice=birth).values('keyword')
+    recomm = []
 
-        for i in keywords:
-            score = 0
-            if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='2', choice=two)).exists():
-                score += 1
-            if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='3', choice=three)).exists():
-                score += 1
-            temp = Rank(score, i['keyword'])
-            recomm.append(temp)
+    for i in keywords:
+        score = 0
+        if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='2', choice=two)).exists():
+            score += 1
+        if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='3', choice=three)).exists():
+            score += 1
+        temp = Rank(score, i['keyword'])
+        print(temp)
+        recomm.append(temp)
 
-        recomm = sorted(recomm, key=lambda rank : rank.score, reverse=False)
-        context = {
-            'recomm': recomm
-        }
-        print(recomm)
+    recomm = sorted(recomm, key=lambda rank : rank.score, reverse=False)
+    print(recomm)
             
 
 
-    return render(request, 'keyword.html', context)
+    return render(request, 'keyword.html')
