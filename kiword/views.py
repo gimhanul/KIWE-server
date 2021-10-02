758f7a9e211feda4a9bb0b872829786367c0fe28
gimhanul <gimhanuly@gmail.com>
2021-10-02 22:01:06 +0900

    Solve: the last error
    now, ycan load keywords when page load

--- kiword/views.py
+++ kiword/views.py
@@ -31,29 +31,30 @@ class Rank():
 
 
 def kiword(request):
-    data = json.loads(request.body)
-    print(data)
-    one = data['one']
-    two = data['two']
-    three = data['three']
-    today = date.today()
-    birth = request.user.birth
-    birth = (today.year - birth.year + 1)//10
-    keywords = KeywordRelated.objects.filter(question='10', choice=birth).values('keyword')
-    recomm = []
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+        one = data['one']
+        two = data['two']
+        three = data['three']
+        today = date.today()
+        birth = request.user.birth
+        birth = (today.year - birth.year + 1)//10
+        keywords = KeywordRelated.objects.filter(question='10', choice=birth).values('keyword')
+        recomm = []
 
-    for i in keywords:
-        score = 0
-        if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='2', choice=two)).exists():
-            score += 1
-        if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='3', choice=three)).exists():
-            score += 1
-        temp = Rank(score, i['keyword'])
-        print(temp)
-        recomm.append(temp)
+        for i in keywords:
+            score = 0
+            if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='2', choice=two)).exists():
+                score += 1
+            if (KeywordRelated.objects.filter(keyword_id=i['keyword'], question='3', choice=three)).exists():
+                score += 1
+            temp = Rank(score, i['keyword'])
+            print(temp)
+            recomm.append(temp)
 
-    recomm = sorted(recomm, key=lambda rank : rank.score, reverse=False)
-    print(recomm)
+        recomm = sorted(recomm, key=lambda rank : rank.score, reverse=False)
+        print(recomm)
             
 
 
