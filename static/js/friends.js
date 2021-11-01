bebd345e1bb5824d455968d796be53e3588193c6
gimhanul <gimhanuly@gmail.com>
2021-11-01 22:34:15 +0900

    Error solve
    Sometimes friend delete request was sended other person, and description more show is not working well

--- static/js/friends.js
+++ static/js/friends.js
@@ -1,18 +1,16 @@
-function show() {
-    des = document.getElementById('description');
-    del = document.getElementById('delete');
-    right = document.getElementById('right');
+function show(id) {
+    des = document.getElementById(id).children[0].children[2].children[0];
+    del = document.getElementById(id).children[0].children[2].children[1];
+    right = document.getElementById(id).children[0].children[2];
 
-    if(des) {
-        del.style.display = 'none';
-        right.style.top = '15%';
-        des.id = 'more-description';
+    if(des.className == "description") {
+        del.style.display = "none";
+        right.style.top = "15%";
+        des.className = "more-description";
     }
-
     else {
-        des = document.getElementById('more-description')
         del.style = "";
         right.style = "";
-        des.id = 'description';
+        des.className = "description";
     }
 }
